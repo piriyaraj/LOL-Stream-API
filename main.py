9ff45b26adae47f220bf5dac76c4790a526f2034
piriyaraj <piriyaraj1998@gmail.com>
2024-08-30 21:09:56 +0530

    Added: Time delay for start game 150 sec

--- main.py
+++ main.py
@@ -117,6 +117,8 @@ if __name__ == "__main__":
                 continue
             else:
                 played_games.append(game_run_command)
+                print("   [-] Time delay: 150 seconds")
+                time.sleep(150)
                 Run(game_run_command,player_team, player_index)
                 
 
